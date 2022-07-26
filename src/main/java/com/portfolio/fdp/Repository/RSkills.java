3c01030c5fcd38372638ba7d200d5278631361f0
Federico Di Cillo <dicillo.fe@gmail.com>
2022-07-26 11:48:56 -0300

    rename

--- src/main/java/com/portfolio/fdp/Repository/RSkills.java
+++ src/main/java/com/portfolio/fdp/Repository/RSkills.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Rskills extends JpaRepository<Skills, Integer> {
+public interface RSkills extends JpaRepository<Skills, Integer> {
 
     public Optional<Skills> findByUnaSkill(String unaSkill);
 
--- src/main/java/com/portfolio/fdp/Service/SSkills.java
+++ src/main/java/com/portfolio/fdp/Service/SSkills.java
@@ -6,14 +6,14 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.portfolio.fdp.Repository.Rskills;
+import com.portfolio.fdp.Repository.RSkills;
 
 @Service
 @Transactional
 public class SSkills {
 
     @Autowired
-    Rskills rSkills;
+    RSkills rSkills;
 
     public List<Skills> list() {
         return rSkills.findAll();
