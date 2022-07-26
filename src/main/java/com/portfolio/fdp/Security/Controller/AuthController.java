039642ef3c3c7d68a88a6f069f7f1816cd462b96
Federico Di Cillo <dicillo.fe@gmail.com>
2022-07-26 13:02:23 -0300

    Update AuthController.java

--- src/main/java/com/portfolio/fdp/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/fdp/Security/Controller/AuthController.java
@@ -22,7 +22,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,7 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping({"/auth"})
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
 
     @Autowired
